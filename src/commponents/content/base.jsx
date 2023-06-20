f3274e8a37465ccede01b107781dd320d7185654
Wei Wei <2312052806@qq.com>
2023-06-20 22:53:02 +0800

    design calculator pattern

--- src/commponents/content/base.jsx
+++ src/commponents/content/base.jsx
@@ -4,7 +4,7 @@ class Base extends Component {
     state = {  } 
     render() { 
         return (
-            <div className='card' style={{ marginTop: "20px" }}>
+            <div className='card' style={{ marginTop: "20px", backgroundColor: "lightblue" }}>
                 <div className='card-body'>
                     { this.props.children }
                 </div>
