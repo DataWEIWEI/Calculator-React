49d51fc226e8dd91260e8aa106e28b4e54786b4a
Wei Wei <2312052806@qq.com>
2023-06-20 10:01:09 +0800

    add pages

---
+++ src/commponents/app.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import NavBar from './navbar';
+
+class App extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <React.Fragment>
+                <NavBar/>
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default App;
